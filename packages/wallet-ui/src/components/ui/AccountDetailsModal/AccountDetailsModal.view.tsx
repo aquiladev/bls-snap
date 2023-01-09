535c16446dc8d05ad6e623ef5f07becab3929726
dmytrobaida <dmytrobaida@outlook.com>
2023-01-09 16:33:10 +0200

    adds qr code image to acc details

--- packages/wallet-ui/src/components/ui/AccountDetailsModal/AccountDetailsModal.view.tsx
+++ packages/wallet-ui/src/components/ui/AccountDetailsModal/AccountDetailsModal.view.tsx
@@ -1,3 +1,5 @@
+import { useContext } from 'react';
+import { ThemeContext } from 'styled-components';
 import {
   AccountImageDiv,
   AccountImageStyled,
@@ -14,6 +16,7 @@ type Props = {
 };
 
 export const AccountDetailsModalView = ({ address }: Props) => {
+  const themeContext = useContext(ThemeContext);
   return (
     <ModalWrapper>
       <AccountImageDiv>
@@ -24,7 +27,11 @@ export const AccountDetailsModalView = ({ address }: Props) => {
           <Title>My account</Title>
           {/* <ModifyIcon /> */}
         </TitleDiv>
-        <AddressQrCode value={address} />
+        <AddressQrCode
+          value={address}
+          bgColor={themeContext.colors.background.default}
+          fgColor={themeContext.colors.text.default}
+        />
         <AddressCopy address={address} />
       </Wrapper>
     </ModalWrapper>
