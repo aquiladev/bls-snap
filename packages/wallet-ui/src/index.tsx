685ecd554b18f18595f7bd7799ef2b32d9a00806
dmytrobaida <dmytrobaida@outlook.com>
2023-01-09 11:25:45 +0200

    adds icon pack

--- packages/wallet-ui/src/index.tsx
+++ packages/wallet-ui/src/index.tsx
@@ -1,10 +1,14 @@
-import React from 'react';
 import { createRoot } from 'react-dom/client';
 import { PersistGate } from 'redux-persist/integration/react';
 import { persistStore } from 'redux-persist';
 import { Provider } from 'react-redux';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { fas } from '@fortawesome/free-solid-svg-icons';
+
 import { store } from './store/store';
 import App from './App';
+
+library.add(fas);
 
 const container = document.getElementById('root') as HTMLElement;
 const root = createRoot(container);
