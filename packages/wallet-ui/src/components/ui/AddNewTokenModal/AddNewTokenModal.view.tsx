3135322ce91125675c048c65465d60df4a629c1a
dmytrobaida <dmytrobaida@outlook.com>
2023-02-21 16:13:26 +0200

    adds text validation

--- packages/wallet-ui/src/components/ui/AddNewTokenModal/AddNewTokenModal.view.tsx
+++ packages/wallet-ui/src/components/ui/AddNewTokenModal/AddNewTokenModal.view.tsx
@@ -18,8 +18,14 @@ export const AddNewTokenModalView = () => {
         <ModalTitle>Add token</ModalTitle>
       </ModalHeaderContainer>
       <AddressInput label="Contract Address" />
-      <ValueInput label="Name" />
-      <ValueInput label="Symbol" />
+      <ValueInput
+        label="Name"
+        validator={(val) => val?.length > 0 && val?.length <= 64}
+      />
+      <ValueInput
+        label="Symbol"
+        validator={(val) => val?.length > 0 && val?.length <= 16}
+      />
       <ValueInput
         label="Decimals"
         validator={(val) => Number.isInteger(Number(val))}
